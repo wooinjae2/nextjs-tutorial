3c8f608b543ce2ca8d5517d7d79b2288e24be924
wooinjae2 <monsterfairy2@gmail.com>
2021-03-17 23:59:26 +0900

    detail api 주소 변경

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -7,7 +7,7 @@ const Post = () => {
     const router = useRouter();
     const { id } = router.query;
     console.log('id출력 : ', id);
-    const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}`;
+    const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
     function getData(){
         axios.get(API_URL).then(res=>{
             console.log(res.data);
